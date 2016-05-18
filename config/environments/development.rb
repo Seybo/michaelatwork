90283997a101383a605e6e68e0ba3a7d0bb5547c
glaux <mirmikh@gmail.com>
2016-05-18 18:22:14 +0300

    ch. make dev reload classes without server restart

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   
   config.cache_classes = false
+  config.reload_classes_only_on_change = false
 
   # Do not eager load code on boot.
   config.eager_load = false
