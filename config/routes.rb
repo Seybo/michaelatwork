fcac87c234e18fc69e454a4c4bbec3796b85ce6c
glaux <mirmikh@gmail.com>
2016-05-19 14:06:58 +0300

    ch. CKeditor cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  mount Ckeditor::Engine => '/ckeditor'
   resources :posts
 #  root 'posts#index'
   
