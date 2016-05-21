9862584e7731db17debdda8a29b3c775a98e6a27
glaux <mirmikh@gmail.com>
2016-05-21 18:44:05 +0300

    ch. add paperclip support to active admin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -2,11 +2,11 @@ ActiveAdmin.register Post do
 
 permit_params :title, :body, :image
 
-form do |f|
+form :html => { :enctype => "multipart/form-data" } do |f|
   f.inputs "Post Details" do
     f.input :title
     f.input :body
-    f.input :image, :as => :file
+    f.input :image, :as => :file, :hint => f.object.image.present? ? f.template.image_tag(f.object.image.url(:thumb)) : content_tag(:span, "no image yet")
   end
   f.actions
 end
@@ -18,7 +18,6 @@ show do |ad|
     row :image do
       image_tag(ad.image.url(:thumb))
     end
-    # Will display the image on show object page
   end
 end
 
