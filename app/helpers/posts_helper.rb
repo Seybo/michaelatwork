ffd14b584cc32a199f1d0715d193267fd855f9c2
glaux <mirmikh@gmail.com>
2016-05-22 10:52:11 +0300

    ft. add truncate post for the index page functionality

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,11 @@
 module PostsHelper
+
+  def truncate_post(post_body, sep = '&sep')
+    post_body.truncate_words(1, {separator: sep})  
+  end
+
+  def remove_separator(post_body, sep = '&sep')
+    post_body.gsub(sep, '')
+  end
+
 end
