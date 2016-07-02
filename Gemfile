987282066abfb340bef94b110eed89e4cb7e1bd9
glaux <mirmikh@gmail.com>
2016-07-02 12:48:53 +0300

    ft. total website redesign in progress

--- Gemfile
+++ Gemfile
@@ -38,6 +38,7 @@ gem 'rouge'
 gem 'friendly_id'
 gem 'slim-rails'
 gem 'html2slim'
+gem 'high_voltage'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   resources :posts
-#  root 'posts#index'
-  
-  root :to => redirect('/posts')
+ 
+  root :to => 'high_voltage/pages#show', id: 'home'
+  get 'development' => 'high_voltage/pages#show', id: 'development'
+  get 'design' => 'high_voltage/pages#show', id: 'design'
+  get 'contact' => 'high_voltage/pages#show', id: 'contact'
   
 end
