8564ee0e18ef2b9f35894acf75af9518369341e4
glaux <mirmikh@gmail.com>
2017-10-15 19:27:19 +0530

    Add other folder to linked dirs

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # config valid only for current version of Capistrano
 lock "3.9.1"
 
@@ -7,7 +9,7 @@ set :repo_url, 'git@github.com:Seybo/michaelatwork.git'
 set :deploy_to, '/home/glaux/space'
 
 append :linked_files, 'config/database.yml', '.env'
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/projects'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/projects', 'public/other'
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.4.1'
